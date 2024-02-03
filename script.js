30d274362d155443fea27d81e819c2caaf03d28e
Will <williamleggiv@gmail.com>
2024-02-03 18:38:40 -0500

    little logic

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function generateSudoku(difficulty) {
     // You can use an external library or write your own logic for Sudoku generation
     // For simplicity, let's assume you have a function generateBoard(difficulty) for this
 
-    board = generateBoard(difficulty);
+    board = fillPuzzle();
 
     const boardContainer = document.getElementById('board-container');
     boardContainer.innerHTML = '';
@@ -31,10 +31,57 @@ function rand_int() {
 
 
 function generateBoard(diff) {
-	let val_board = [];
 	
+	const nextEmptyCell = puzzleArray => {
+	const emptyCell = {rowIndex: "", colIndex: ""}
+
+	puzzleArray.forEach( (row, rowIndex) => {
+	if (emptyCell.colIndex !== "" ) return
+
+	let firstZero = row,find( col => col === 0)
+
+	if (firstZero === undefined) return
+	emptyCell.rowIndex = rowIndex
+	emptyCell.colIndex = row.indexOf(firstZero)
+	})
+
+	if (emptyCell.colIndex !== "") return emptyCell
+
+	return false
+	}
+
+	const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+	const shuffle = array => {
+		let newArray = [...array]
+		for (let i = newArray.lenghth - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[ newArray[i], newArray[j] ] = [newArray[j], newArray[i] ];
+		}
+		return newArray
+	}
+
+	const fillPuzzle = startingBoard => {
+		const emptyCell = nextEmptyCell(startingBoard)
+		return startingBoard if (!emptyCell)
+
+		for (num of shuffle(numArray)) {
+			counter++
+			if (counter > 20_000_000) throw new Error ("Recursion Timeout")
+
+			if (safeToPlace( startingBoard, emptyCell, num)) {
+				startingBoard[ emptyCell.rowIndex ][ emptyCell.colIndex ] = num
+
+				return startingBoard if (fillPuzzle(startingBoard))
+
+				startingBoard[ emptyCell.rowIndex ][ emptyCell.colIndex ] = 0
+			}
+		}
+		return false
+	}
 
 }
+
 
 // Function to check the solution
 function checkSolution() {
